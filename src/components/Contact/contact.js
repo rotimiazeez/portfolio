f03bb30a30c7a1e806b6e7f364484605fb1b020e
rotimiazeez <harzeyzhrotimi@gmail.com>
2022-04-28 18:17:42 +0100

    feat: Create contact page with necessary stylings

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,9 +1,121 @@
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters/animated'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import './contact.scss'
+import { useEffect, useState } from 'react'
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
-    return (
-        <>
-          <div className='container contact-page'></div>  
-        </>
-    )
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const form = useRef()
+
+    useEffect(() => {
+        return setTimeout(() => {
+          setLetterClass('text-animate-hover')
+        }, 3000)
+      }, [])
+
+      const sendEmail = (e) => {
+        e.preventDefault()
+    
+        emailjs
+          .sendForm(
+            'service_m3bnyem',
+            'template_ua7le23',
+            form.current,
+            'XoYJvMM0-ww8pV-VD'
+          )
+          .then(
+            () => {
+              alert('Message successfully sent!')
+              window.location.reload(false)
+            },
+            () => {
+              alert('Failed to send the message, please try again')
+            }
+          )
+      }
+
+  return (
+    <>
+      <div className='container contact-page'>
+          <div className='text-zone'>
+            <h1>
+              <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}/>
+            </h1>
+            <p>
+            I am interested in freelance opportunities - especially ambitious or
+            large projects. However, if you have other request or question,
+            don't hesitate to contact me using below form either.
+          </p>
+          <div className="contact-form">
+            <form ref={form} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input placeholder="Name" type="text" name="name" required />
+                </li>
+                <li className="half">
+                  <input
+                    placeholder="Email"
+                    type="email"
+                    name="email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+          </div>
+        <div className="info-map">
+          Azeez Rotimi,
+          <br />
+          Nigeria,
+          <br />
+          Lagos mainland, <br />
+          Ogudu, Lagos <br />
+          <br />
+          <span>harzeyzhrotimi@gmail.com </span>
+        </div>
+        <div className="map-wrap">
+        <MapContainer center={[6.572, 3.396]} zoom={13} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[6.572, 3.396]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+         </div>
+        </div>
+      <Loader type='pacman' />
+    </>
+  )
 }
+
+
+export default Contact
