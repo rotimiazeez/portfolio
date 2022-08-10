819e97dc1a57e6759bcade205c0bf89725dbfc58
rotimiazeez <harzeyzhrotimi@gmail.com>
2022-08-10 22:32:57 +0100

    feat - Modify layout style

--- src/components/Sidenav/nav.js
+++ src/components/Sidenav/nav.js
@@ -9,7 +9,7 @@ import {
     faTwitter,
     faSkype,
   } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Sidenav = () => (
     <div className='nav-bar'>
@@ -23,6 +23,9 @@ const Sidenav = () => (
             </NavLink>
             <NavLink  activeclassname='active' className='about-link' to='/about'>
                 <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
+            </NavLink>
+            <NavLink  activeclassname='active' className='portfolio-link' to='/portfolio'>
+                <FontAwesomeIcon icon={faSuitcase} color='#4d4d4e' />
             </NavLink>
             <NavLink  activeclassname='active' className='contact-link' to='/contact'>
                 <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
